9b2a216b638c886c907b9c3f5b67ed578ff962da
Bach Le <lehoangbach7802@gmail.com>
2022-05-17 10:07:51 -0400

    commit tree b4e86b67633003a2c9e5f1eea979d40e63719a43

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if(someValue <= 400) {
+    return 'This one is on me!'
+  }
+
+  else if(someValue <= 2000) {
+    return 'That will be twenty bucks.'
+  }
+
+  else if(someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+
+  else if(someValue > 2500) {
+    return 'No can do.'
+  }
+};
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let reply;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip) {
+    case tip = 'generous':
+      reply = ('Thank you so much.');
+      break;
+    case tip = 'not as generous':
+      reply = ('Thank you.');
+      break;
+    case tip = 'thanks for everything':
+      reply = ('Bye.');
+      break;
+  }
+  return reply;
 }
